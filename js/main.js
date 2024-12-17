04ee04ae068a767ed4de6d9311cdc142e1656e04
drusjs <magnojoky962@gmail.com>
2024-12-17 11:49:24 +0300

    add color check + uislider

--- js/main.js
+++ js/main.js
@@ -38,3 +38,30 @@ if (materialTab.length > 0) {
         })
     })
 }
+
+const colorLabels = document.querySelectorAll('.color-label')
+const showButton = document.querySelectorAll('.text-button-show')
+
+if (colorLabels.length > 0) {
+    colorLabels.forEach(label => {
+        const color = label.dataset.color
+        const shadow = `0 0 0 1px ${color}`
+        
+        label.style.backgroundColor = color
+        label.style.boxShadow = shadow
+    })
+}
+
+if (showButton.length > 0) {
+    showButton.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.classList.toggle('active')
+            
+            if (e.currentTarget.parentElement.classList.contains('active')) {
+                e.currentTarget.closest('.catalog-filter-item-dropdown').style.height = e.currentTarget.closest('.catalog-filter-item-dropdown').scrollHeight + 'px'
+            } else {                
+                e.currentTarget.closest('.catalog-filter-item-dropdown').style.height = '30px'
+            }
+        })
+    })
+}
